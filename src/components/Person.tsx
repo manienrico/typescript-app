8b980304edbd2b6bf5652f9965ef91026751d6c2
Enrico Mani <manienrico13@gmail.com>
2023-05-31 10:19:38 +0300

    setting state and handling events in typescript

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,4 +1,4 @@
-import {FC} from 'react';
+import {FC,ChangeEvent,useState} from 'react';
 
 interface Props {
     name: string;
@@ -9,14 +9,24 @@ interface Props {
 
 export const Person:FC<Props> = ({name,age,email}) => {
 
-    //const [country,setCountry] = useState("")
+    const [country,setCountry] = useState<string | null>("")
 
+    const handleChange = (event: ChangeEvent<HTMLInputElement>)=>{
+        setCountry(event.target.value)
+    }
 
     return (
     <div>
       <h1>{name}</h1>
       <h1>{email}</h1>
       <h1>{age}</h1>
+      <input
+       type="text" 
+       placeholder='Write your country'
+       onChange={handleChange}
+
+        />
+      {country}
     </div>
   );
 }
